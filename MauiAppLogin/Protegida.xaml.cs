be46cb47cea18b2da68ff7f208d46c39dff4448e
Kauê Neto <kauecarddoso@gmail.com>
2025-08-31 14:26:21 -0300

    project complete

--- MauiAppLogin/Protegida.xaml.cs
+++ MauiAppLogin/Protegida.xaml.cs
@@ -1,9 +1,28 @@
-namespace MauiAppLogin;
+﻿namespace MauiAppLogin;
 
 public partial class Protegida : ContentPage
 {
-	public Protegida()
-	{
-		InitializeComponent();
-	}
+    public Protegida()
+    {
+        InitializeComponent();
+
+        string? usuario_logado = null;
+
+        Task.Run(async () =>
+        {
+            usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+            lbl_boasvindas.Text = $"Bem-vindo (a) {usuario_logado}";
+        });
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        bool confirmacao = await DisplayAlert("Tem Certeza?", "Sair da conta?", "Sim", "N�o");
+
+        if (confirmacao)
+        {
+            SecureStorage.Default.Remove("usuario_logado");
+            App.Current.MainPage = new Login();
+        }
+    }
 }
